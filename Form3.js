fc40a46a7f3ef8b6bce30b95e268981f954ae4a3
AbhigyanKundu <69205297+AbhigyanKundu@users.noreply.github.com>
2021-02-26 14:59:27 +0530

    Add files via upload

---
+++ Form3.js
@@ -0,0 +1,36 @@
+class Form3 {
+
+    constructor() {
+        this.input = createInput("Place of stay eg.Navi Mumbai,Koperkhairane");
+        this.input = createInput("Adress");
+        this.input = createInput("Phone Number");
+    //  this.input = createInput("Name");
+   // this.button = createButton('Get Started');// In case of play get started
+
+    //     this.emergency1= createButton('Police Station');
+    //     this.emergency2= createButton('Fire Brigade');
+    //     this.emergency3= createButton('Ambulance');
+    //     this.emergency4= createButton('Resturants');
+    //     this.emergency5= createButton('Hospitals');
+    //  //   this.greeting = createElement('h2');
+       // this.title = createElement('h2');
+    }
+    display(){
+
+        this.button.mousePressed(()=>{
+            //I want to give function that if user presses the emergency1 button input should display   // this.input.hide();
+   
+   
+            // this.button.hide();
+            // player.name = this.input.value();
+            // playerCount+=1;
+            // player.index = playerCount;
+            // player.update();
+            // player.updateCount(playerCount);
+            // this.greeting.html("Hello " + player.name)
+            // this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+          });
+          
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# My_App_Stage_3
+# p5.play-boilerplate
+Boiler plate for p5.play
+# At last I want to connect it to Google frm which i want to give direction,phone no. etc.
